864b06372c9fae2b980ef70a2ba22ffd05d34b0b
hleu0019 <ryanleung1029@gmail.com>
2023-05-20 00:00:33 +1000

    play button added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ clock = pygame.time.Clock()
 BLACK, WHITE = (0, 0, 0), (255, 255, 255)
 GRAVITY = 1
 FPS = 60
+BUTTON_SIZE=[screen_width//3, 100]
 
 
 class Ball(object):
@@ -59,28 +60,41 @@ class Ball(object):
 def game():
     running = True
     ball_radius = 15
-    balls = [ Ball(
+    play = False
+
+    balls = [Ball(
                 randint(ball_radius + 1, screen_width - ball_radius - 1), 
                 randint(ball_radius + 1, screen_height - ball_radius - 1), 
                 20, 20, ball_radius )  for _ in range(30)]
 
     while running:
+        mouse = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if (screen_width//3 <= mouse[0] <= 2*screen_width//3) and (screen_height//6 <= mouse[1] <= screen_height//6 + BUTTON_SIZE[1]):
+                    play = True
         screen.fill(BLACK)
 
-        line_y = int(2 * screen_height / 3) - 10
-        line_width = 10
-        pygame.draw.line(screen, WHITE, (0, line_y), (screen_width, line_y), line_width)
+        if not play:
+            if (screen_width//3 <= mouse[0] <= 2*screen_width//3) and (screen_height//6 <= mouse[1] <= screen_height//6 + BUTTON_SIZE[1]):
+                pygame.draw.rect(screen,(177,177,177), ((screen_width//3,screen_height//6), (BUTTON_SIZE[0],BUTTON_SIZE[1])))
+            else:
+                pygame.draw.rect(screen,(100,100,100), ((screen_width//3,screen_height//6), (BUTTON_SIZE[0],BUTTON_SIZE[1])))
 
-        for ball in balls:
-            if(ball.x - ball.r <= 0) or (ball.x + ball.r >= screen_width):
-                ball.x_collide()
-            elif  (ball.y + ball.r >= line_y):
-                ball.y_collide()
-            ball.move()
-            ball.draw()
+        if play:
+            line_y = int(2 * screen_height / 3) - 10
+            line_width = 10
+            pygame.draw.line(screen, WHITE, (0, line_y), (screen_width, line_y), line_width)
+
+            for ball in balls:
+                if(ball.x - ball.r <= 0) or (ball.x + ball.r >= screen_width):
+                    ball.x_collide()
+                elif  (ball.y + ball.r >= line_y):
+                    ball.y_collide()
+                ball.move()
+                ball.draw()
 
         pygame.display.flip()
         pygame.display.update()
