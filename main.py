b63df1d3291e0353ce80222bf1c1fa1682b0bf75
hleu0019 <ryanleung1029@gmail.com>
2023-05-20 02:03:13 +1000

    comments

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
-from random import randint, uniform, seed
+from random import randint, uniform
 
+# * Initialise game
 pygame.init()
+pygame.font.init()
+font = pygame.font.SysFont('Comic Sans MS', 30)
 clock = pygame.time.Clock()
-screen_width, screen_height = 800, 800
+screen_width, screen_height = 1500, 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Bouncing Balls")
 clock = pygame.time.Clock()
 
+# * Game constants
 BLACK, WHITE = (0, 0, 0), (255, 255, 255)
+GREY, LIGHT_GREY = (177,177,177), (100,100,100)
 GRAVITY = 1
 FPS = 60
 BUTTON_SIZE=[screen_width//3, 100]
@@ -32,6 +37,7 @@ class Ball(object):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
 
     def move(self):
+        # * Limit acceleration to only 2 to slow game down
         if self.ya < 2:
             self.yv = self.yv + self.ya
             self.ya += GRAVITY
@@ -43,16 +49,19 @@ class Ball(object):
         self.x += self.xv
 
     def x_collide(self):
+        # * Handle collisions on sides
         self.xv *= -uniform(0.9, 1)
         self.xa *= -uniform(0.9, 1)
         self.bounces += 1
 
     def y_collide(self):
+        # * Handle collisions on line
         self.yv *= -uniform(0.9, 1)
         self.xa *= -uniform(0.9, 1)
         self.bounces += 1
 
     def set_stationary(self):
+        # todo implement ball rolling
         self.yv, self.ya = 0,0
 
 
@@ -61,7 +70,7 @@ def game():
     running = True
     ball_radius = 15
     play = False
-
+    txt = font.render("Random", True, (255,255,255))
     balls = [Ball(
                 randint(ball_radius + 1, screen_width - ball_radius - 1), 
                 randint(ball_radius + 1, screen_height - ball_radius - 1), 
@@ -75,15 +84,20 @@ def game():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if (screen_width//3 <= mouse[0] <= 2*screen_width//3) and (screen_height//6 <= mouse[1] <= screen_height//6 + BUTTON_SIZE[1]):
                     play = True
+                # todo more buttons
         screen.fill(BLACK)
 
         if not play:
+            # * Show game options
             if (screen_width//3 <= mouse[0] <= 2*screen_width//3) and (screen_height//6 <= mouse[1] <= screen_height//6 + BUTTON_SIZE[1]):
-                pygame.draw.rect(screen,(177,177,177), ((screen_width//3,screen_height//6), (BUTTON_SIZE[0],BUTTON_SIZE[1])))
+                pygame.draw.rect(screen, GREY, ((screen_width//3,screen_height//6), (BUTTON_SIZE[0],BUTTON_SIZE[1])))
             else:
-                pygame.draw.rect(screen,(100,100,100), ((screen_width//3,screen_height//6), (BUTTON_SIZE[0],BUTTON_SIZE[1])))
-
-        if play:
+                pygame.draw.rect(screen, LIGHT_GREY, ((screen_width//3,screen_height//6), (BUTTON_SIZE[0],BUTTON_SIZE[1])))
+            screen.blit(txt,(200+screen_width//3, screen_height//6 + 25))
+            # todo add more options
+        
+        else:
+            # * Start game
             line_y = int(2 * screen_height / 3) - 10
             line_width = 10
             pygame.draw.line(screen, WHITE, (0, line_y), (screen_width, line_y), line_width)
@@ -95,6 +109,7 @@ def game():
                     ball.y_collide()
                 ball.move()
                 ball.draw()
+            # todo implement rolling
 
         pygame.display.flip()
         pygame.display.update()
