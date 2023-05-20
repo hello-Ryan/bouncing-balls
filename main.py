a4e491b8d0461fdac3b1ae88bcae24334a32f856
hleu0019 <ryanleung1029@gmail.com>
2023-05-21 01:56:38 +1000

    different modes wip

--- main.py
+++ main.py
@@ -20,32 +20,30 @@ BUTTON_SIZE=[screen_width//3, 100]
 
 
 class Ball(object):
-    def __init__(self, x, y, width, height, r):
+    def __init__(self, x, y, r):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
         self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
         self.r = r
         self.yv = 0
         self.ya = 0
         self.xa = 0
-        self.xv = randint(1,10)
+        self.xv = randint(-10,10)
         self.bounces = 0
 
     def draw(self):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+        # ! always spawning at the top of the screen for some reason
+        pygame.draw.circle(screen, self.color, (self.y, self.y), self.r)
 
     def move(self):
-        # * Limit acceleration to only 2 to slow game down
-        if self.ya < 2:
+        # * Limit acceleration to only 1 to slow game down
+        if self.ya < 1:
             self.yv = self.yv + self.ya
             self.ya += GRAVITY
             self.y = self.yv
         else:
             self.yv = self.yv + self.ya
             self.y += self.yv
-
         self.x += self.xv
 
     def x_collide(self):
@@ -71,16 +69,19 @@ def game():
     ball_radius = 15
     play_random = False
     play_custom = False
+    start_custom = False
     txt = font.render("Random", True, (255,255,255))
     txt2 = font.render("Custom", True, (255,255,255))
     txt3 = font.render("Quit", True, (255,255,255))
-    balls = [Ball(
+    random_balls = [Ball(
                 randint(ball_radius + 1, screen_width - ball_radius - 1), 
                 randint(ball_radius + 1, screen_height - ball_radius - 1), 
-                20, 20, ball_radius )  for _ in range(30)]
+                ball_radius )  for _ in range(30)]
     
+    custom_balls = []
 
 
+    # * Game loop
     while running:
         mouse = pygame.mouse.get_pos()
         btn1_condition = (screen_width // 3 <= mouse[0] <= 2 * screen_width // 3) and (screen_height // 6 <= mouse[1] <= screen_height // 6 + BUTTON_SIZE[1])
@@ -91,13 +92,17 @@ def game():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if btn1_condition:
-                    play_random = True
-                elif btn2_condition:
-                    play_custom = True
-                elif btn3_condition:
-                    running = False
-                    
+                if not (play_random or play_custom):
+                    if btn1_condition:
+                        play_random = True
+                    elif btn2_condition:
+                        play_custom = True
+                    elif btn3_condition:
+                        running = False
+
+                elif play_custom:
+                    custom_balls.append(Ball(mouse[0],mouse[1],15))
+
         screen.fill(BLACK)
 
         if not (play_random or play_custom):
@@ -122,23 +127,38 @@ def game():
                 pygame.draw.rect(screen, GREY, ((screen_width // 3, 2 * screen_height // 6), (BUTTON_SIZE[0], BUTTON_SIZE[1])))
                 pygame.draw.rect(screen, GREY, ((screen_width // 3, 3 * screen_height // 6), (BUTTON_SIZE[0], BUTTON_SIZE[1])))
                 
+            # * Write text on screen
             screen.blit(txt,(200 + screen_width // 3, screen_height // 6 + 25))
             screen.blit(txt2,(200 + screen_width // 3, 2 * screen_height // 6 + 25))
             screen.blit(txt3,(200 + screen_width // 3, 3 * screen_height // 6 + 25))
-        
+
+
         else:
-            # * Start game
             line_y = int(2 * screen_height / 3) - 10
             line_width = 10
-            pygame.draw.line(screen, WHITE, (0, line_y), (screen_width, line_y), line_width)
 
-            for ball in balls:
-                if(ball.x - ball.r <= 0) or (ball.x + ball.r >= screen_width):
-                    ball.x_collide()
-                elif  (ball.y + ball.r >= line_y):
-                    ball.y_collide()
-                ball.move()
-                ball.draw()
+            if play_random:
+                # * random game
+                pygame.draw.line(screen, WHITE, (0, line_y), (screen_width, line_y), line_width)
+                for ball in random_balls:
+                    if(ball.x - ball.r <= 0) or (ball.x + ball.r >= screen_width):
+                        ball.x_collide()
+                    elif  (ball.y + ball.r >= line_y):
+                        ball.y_collide()
+                    ball.move()
+                    ball.draw()
+
+            elif play_custom:
+                # * custom game
+                pygame.draw.line(screen, WHITE, (0, line_y), (screen_width, line_y), line_width)
+                for ball in custom_balls:
+                    if(ball.x - ball.r <= 0) or (ball.x + ball.r >= screen_width):
+                        ball.x_collide()
+                    elif  (ball.y + ball.r >= line_y):
+                        ball.y_collide()
+                    ball.move()
+                    ball.draw()
+
             # todo implement rolling
 
         pygame.display.flip()
